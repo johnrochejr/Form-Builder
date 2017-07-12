cf22354de93b20d6003d4a1771a18853dd4deddf
John Roche <roche.johnjr@gmail.com>
2017-07-12 13:57:06 -0400

    Completed Mark walkthrough

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let formData = [
   // compare the input in the HTML file with the
   //contents of this first object
   {
-    "type": "text",
+    "type": "text", // [0]
     "label": "First Name",
     "id": "user-first-name",
     "icon": "fa-user",
@@ -98,30 +98,80 @@ let formData = [
 
 // -------- Your Code Goes Below this Line --------
 
-let createInput;
-let createSelect;
 
-function creatingInput() {
-  for (let i = 0; i < formData.length; i++) {
-    createInput = document.createElement("input");
-    if (formData.type === "select") {
-      createSelect = document.createElement("select");
-      createSelect.type = formData.type;
+/* Loop through the form Data
+  create the HTML (command)
+  add to the DOM (command) */
 
-      let selectLanguage = document.createElement("option");
-        selectLanguage.text = "Select languate...";
-        selectLanguage.selected = "selected";
-        createSelect.appendChild(selectLanguage);
+  let _fields = document.getElementById('fields');
+  // HTML selector
+  // This variable will cut down on us typing document.querySelector("#fields")
+  // every time we need it
 
+  for (var i = 0; i < formData.length; i++) {
+    // I do this so I don't have to type formData[i] a lot
+    let _data = formData[i];
 
-
+    console.log(`creating HTML for ${_data.label}`);
 
 
+    // only for the comment box
+    if (_data.type === 'textarea') {
+      // TODO: create and append textarea
+      // Create a new element that is a textarea
+      // Why? Because the type of the input is 'textarea'
+      let textarea = document.createElement('textarea');
+      textarea.placeholder = _data.label;
+      textarea.id = _data.id;
 
+      _fields.appendChild(textarea);
+
+    } else if (_data.type === "select") { // create a dropdown
+      // create select elements
+      let select = document.createElement('select');
+      select.id = _data.id;
+
+      // create our placeholder for 'Select language...'
+
+      let placeHolder = document.createElement('option');
+      placeHolder.value = 0;
+      placeHolder.innerHTML = _data.label;
+
+       select.appendChild(placeHolder);
+
+      for (var j = 0; j < _data.options.length; j++) { // need to choose index different than "i"
+        let _opt = _data.options[j]
+        let option = document.createElement('option');
+        option.value = _opt.value;
+        option.innerHTML = _opt.label; //set content of an HTML
+        select.appendChild(option);
+      }
+
+      // loop over the options and create an element for each options
+      // append to the select
+
+      _fields.appendChild(select);
+
+
+
+    } else { // this is our base case
+      let element = document.createElement('input');
+      // Create variable here so I can reuse and modify that new element
+      // I called this variable 'element' because the name is logical and
+      // has semantic meaning
+      element.type = _data.type;
+      // to set HTML attributes on a newly created or queried DOM element,
+      // the formula is element.attribute = value
+      element.placeholder = _data.label;
+      element.id = _data.id;
+
+      _fields.appendChild(element);
+      // select the ID in HTML where you want to add element
+      // Set properties first, then appendChild to DOM
     }
-  }
-}
 
+
+  }
 
 
 
